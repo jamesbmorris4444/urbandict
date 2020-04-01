4e09cd9c92073dbffd408160e3983b8e0e3b137f
jmorris <jim.morris@mobilepointofsales.com>
2020-03-31 19:42:00 -0500

    master commit 029

--- app/src/main/java/com/fullsekurity/urbandict/meanings/MeaningsItemViewModel.kt
+++ app/src/main/java/com/fullsekurity/urbandict/meanings/MeaningsItemViewModel.kt
@@ -2,34 +2,30 @@ package com.fullsekurity.urbandict.meanings
 
 import androidx.databinding.ObservableField
 import com.fullsekurity.urbandict.activity.Callbacks
-import com.fullsekurity.urbandict.logger.LogUtils
 import com.fullsekurity.urbandict.recyclerview.RecyclerViewItemViewModel
 import com.fullsekurity.urbandict.repository.storage.Meaning
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
 
 class MeaningsItemViewModel(private val callbacks: Callbacks) : RecyclerViewItemViewModel<Meaning>() {
 
     val definition: ObservableField<String> = ObservableField("")
-    val permalink: ObservableField<String> = ObservableField("")
+//    val permalink: ObservableField<String> = ObservableField("")
     val thumbsUp: ObservableField<String> = ObservableField("")
     val author: ObservableField<String> = ObservableField("")
     val word: ObservableField<String> = ObservableField("")
-    val defId: ObservableField<String> = ObservableField("")
-    val currentVote: ObservableField<String> = ObservableField("")
+//    val defId: ObservableField<String> = ObservableField("")
+//    val currentVote: ObservableField<String> = ObservableField("")
     val writtenOn: ObservableField<String> = ObservableField("")
     val example: ObservableField<String> = ObservableField("")
     val thumbsDown: ObservableField<String> = ObservableField("")
 
     override fun setItem(item: Meaning) {
         definition.set(item.definition)
-        permalink.set(item.permalink)
+//        permalink.set(item.permalink)
         thumbsUp.set(item.thumbsUp.toString())
         author.set("By: ${item.author}")
         word.set(item.word)
-        defId.set(item.defId.toString())
-        currentVote.set(item.currentVote)
+//        defId.set(item.defId.toString())
+//        currentVote.set(item.currentVote)
         val index = item.writtenOn.indexOf('T')
         var date = "NO DATE"
         if (index == 10) {
